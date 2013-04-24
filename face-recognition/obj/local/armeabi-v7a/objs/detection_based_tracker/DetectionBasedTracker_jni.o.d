93493cee668bfb0c7e4a3116fbb0776a13d55c47
almde89 <almde89@gmail.com>
2013-04-24 18:09:18 -0300

    Será a base para o nosso projeto. Teremos somente que alterar esse sample do opencv.

---
+++ face-recognition/obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o.d
@@ -0,0 +1,145 @@
+obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o: \
+ jni/DetectionBasedTracker_jni.cpp jni/DetectionBasedTracker_jni.h \
+ /usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/jni.h \
+ /home/andremello/Downloads/OpenCVPacks/OpenCV-2.4.5-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp \
+ /home/andremello/Downloads/OpenCVPacks/OpenCV-2.4.5-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h \
+ /usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/assert.h \
+ /usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ /usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ /usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ /usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/stdlib.h \
+ /usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/string.h \
+ /usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/malloc.h \
+ /usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/alloca.h \
+ /usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/strings.h \
+ /usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ /usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/stdint.h \
+ /usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ /usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ /usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ /usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ /usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ /usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ /usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ /usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ /usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ /usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ /usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/memory.h \
+ /usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/math.h \
+ /usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/limits.h \
+ /usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/limits.h \
+ /usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/linux/limits.h \
+ /usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/machine/internal_types.h \
+ /usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/machine/limits.h \
+ /usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/syslimits.h \
+ /usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/asm/page.h \
+ /home/andremello/Downloads/OpenCVPacks/OpenCV-2.4.5-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp \
+ /usr/lib/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm \
+ /usr/lib/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath \
+ /usr/lib/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef \
+ /usr/lib/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/complex \
+ /usr/lib/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/map \
+ /usr/lib/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/vector \
+ /home/andremello/Downloads/OpenCVPacks/OpenCV-2.4.5-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp \
+ /usr/lib/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/limits \
+ /home/andremello/Downloads/OpenCVPacks/OpenCV-2.4.5-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp \
+ /home/andremello/Downloads/OpenCVPacks/OpenCV-2.4.5-android-sdk/sdk/native/jni/include/opencv2/contrib/detection_based_tracker.hpp \
+ /home/andremello/Downloads/OpenCVPacks/OpenCV-2.4.5-android-sdk/sdk/native/jni/include/opencv2/objdetect/objdetect.hpp \
+ /usr/lib/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/deque \
+ /usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/android/log.h
+
+jni/DetectionBasedTracker_jni.h:
+
+/usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/jni.h:
+
+/home/andremello/Downloads/OpenCVPacks/OpenCV-2.4.5-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp:
+
+/home/andremello/Downloads/OpenCVPacks/OpenCV-2.4.5-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h:
+
+/usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/assert.h:
+
+/usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+/usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+/usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/stdlib.h:
+
+/usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/string.h:
+
+/usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+/usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/alloca.h:
+
+/usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/strings.h:
+
+/usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+/usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+/usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+/usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+/usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+/usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+/usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+/usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+/usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+/usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+/usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+/usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+/usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/memory.h:
+
+/usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/math.h:
+
+/usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/limits.h:
+
+/usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/limits.h:
+
+/usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/linux/limits.h:
+
+/usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/machine/internal_types.h:
+
+/usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/machine/limits.h:
+
+/usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/sys/syslimits.h:
+
+/usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/asm/page.h:
+
+/home/andremello/Downloads/OpenCVPacks/OpenCV-2.4.5-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp:
+
+/usr/lib/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm:
+
+/usr/lib/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath:
+
+/usr/lib/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef:
+
+/usr/lib/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/complex:
+
+/usr/lib/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/map:
+
+/usr/lib/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/vector:
+
+/home/andremello/Downloads/OpenCVPacks/OpenCV-2.4.5-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp:
+
+/usr/lib/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/limits:
+
+/home/andremello/Downloads/OpenCVPacks/OpenCV-2.4.5-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp:
+
+/home/andremello/Downloads/OpenCVPacks/OpenCV-2.4.5-android-sdk/sdk/native/jni/include/opencv2/contrib/detection_based_tracker.hpp:
+
+/home/andremello/Downloads/OpenCVPacks/OpenCV-2.4.5-android-sdk/sdk/native/jni/include/opencv2/objdetect/objdetect.hpp:
+
+/usr/lib/android-ndk-r8e/sources/cxx-stl/gnu-libstdc++/4.6/include/deque:
+
+/usr/lib/android-ndk-r8e/platforms/android-8/arch-arm/usr/include/android/log.h:
